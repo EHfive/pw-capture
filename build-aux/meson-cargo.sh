a2842f1680d6266581b06138c53ca03807a5c3a1
Huang-Huang Bao <i@eh5.me>
2024-12-09 22:05:54 +0800

    improve(build): allow build with --frozen option

--- build-aux/meson-cargo.sh
+++ build-aux/meson-cargo.sh
@@ -7,7 +7,8 @@ PROFILE="$3"
 PACKAGE="$4"
 FILENAME="$5"
 OUTPUT="$6"
-RUST_TARGET="$7" # last argument, optional
+FROZEN="$7"
+RUST_TARGET="$8" # last argument, optional
 
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
 export CARGO_HOME="$MESON_BUILD_ROOT"/cargo-home
@@ -19,12 +20,19 @@ if [[ "$PROFILE" != "dev" ]]; then
     PROFILE_DIR="$PROFILE"
 fi
 
-if [[ -n "$RUST_TARGET" ]]; then
-    ARGS+=( '--target' "$RUST_TARGET" )
+if [[ "$FROZEN" == "true" ]]; then
+    ARGS+=('--frozen')
 fi
 
+if [[ -n "$RUST_TARGET" ]]; then
+    ARGS+=('--target' "$RUST_TARGET")
+fi
 
-cargo build --profile "$PROFILE" "${ARGS[@]}" --manifest-path="$MESON_SOURCE_ROOT"/Cargo.toml -p "$PACKAGE"
+cp "$MESON_SOURCE_ROOT"/Cargo.lock ./
+
+cargo build --profile "$PROFILE" "${ARGS[@]}" \
+    --manifest-path="$MESON_SOURCE_ROOT"/Cargo.toml \
+    -p "$PACKAGE"
 
 if [[ -n "$RUST_TARGET" ]]; then
     cp "${CARGO_TARGET_DIR}/${RUST_TARGET}/${PROFILE_DIR}/${FILENAME}" "$OUTPUT"
